347d1d087fa4bd26e044814d2bad8cc7e5a2f14c
steeple <steeplew@gmail.com>
2015-10-06 16:30:38 +0300

    up

---
+++ Bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace steeple\meta;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ * @package steeple\meta
+ */
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        var_dump($app);
+        exit;
+    }
+}
